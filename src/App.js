3679832f4dfbcf0d79c0e0d45397fab93d5ed467
scott246 <scott246@purdue.edu>
2017-09-24 15:24:49 -0400

    made a header and menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MainTile from './MainTile.js';
 import './App.css';
+import appjson from './apps.json';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Dropdown">
+        <button className="Dropbtn"> All Apps </button>
+        <div className="Dropdown-content">
+        {
+          appjson.apps.map((item) => {
+            return <Row key={item.name} label={item.name} href={item.href} />
+          })
+        }
+        </div>
+      </div>
+    )
+  }
+}
+
+class Row extends Component {
+  render() {
+    return (
+      <a href={this.props.href}>{this.props.label}</a>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="Name">
+            <h3>Annie{'\''}s List</h3>
+          </div>
+          <Navbar />
         </div>
+        <MainTile />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          "Made with {'<'}3 by Nathan Scott, 2017."
         </p>
       </div>
     );
